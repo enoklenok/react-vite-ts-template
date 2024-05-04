fc6f7369b8831345367841a075a2a4998218d39d
enoklenok <sinyaenok@gmail.com>
2024-05-04 15:10:20 +0900

    [ADD]tsconfig, alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@routes", replacement: "/src/routes" },
+      { find: "@store", replacement: "/src/store" },
+      { find: "@styles", replacement: "/src/styles" },
+      { find: "@types", replacement: "/src/types" },
+      { find: "@utils", replacement: "/src/utils" },
+    ],
+  },
+});
